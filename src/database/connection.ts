b27d45744a791b99982e3ab97924d22be6873560
Gabriel Moura <gabriel.brito@linkapi.com.br>
2020-11-28 16:29:39 -0300

    build: create DB connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
-createConnection();
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+
+connection.on('error', console.error.bind(console, 'Connection error:'));
+
+connection.once('open', () => console.log('Connected to MongoDB!'));
+
+export { connection };

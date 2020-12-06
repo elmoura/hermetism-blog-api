6a7a46ce808688a04412103d055d0a759bf18209
Gabriel Moura <gabriel.brito@linkapi.com.br>
2020-12-06 09:52:32 -0300

    feat: adicionar rotas de autores

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import { postRoutes } from './post.routes';
 import { usersRoutes } from './users.routes';
 import { authorsRoutes } from './authors.routes';
 
 const routes = Router();
 
+routes.use('/posts', postRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/authors', authorsRoutes);
 
